d76c308bd8a81ea0c4bfa4403097377c844a7a96
TOPIC <t.wanitcharoennan@gmail.com>
2023-02-09 11:24:45 +0700

    adop

---
+++ adop/adop.js
@@ -0,0 +1,32 @@
+// secure2.playpark.com
+$("#secure2_adop").append(` 
+<div id='adop_bfd' style='display:block;width:100%;position:fixed;bottom:0px;right:0px;z-index:999;text-align:center;background:rgb(255, 255, 255, 0.85);box-shadow:rgba(0, 0, 0, 0.25) 0px 0px 7px;border-top:4px solid #353535;'>
+<script  src='https://compass.adop.cc/assets/js/adop/adop.js?v=14' ></script><ins class='adsbyadop' _adop_zon = '14ca7a2d-25c3-49af-9308-2eb1cf7e543a' _adop_type = 'rs' style='display:inline-block;width:320px;height:100px;' _page_url='' _over_size='auto'_over_zone='{"728x90":"91924cb2-ff6f-4ee9-bf61-5924aab5d4f8"}' ></ins>
+<span id='adop_banner_x' style='position:absolute;bottom:100%;right:0;padding:2px 8px;background:#060102;font-size:12px;color:#ffffff;border-bottom:4px solid #353535;'>X</span>
+<script>let adop_closebfd = function () {document.getElementById('adop_bfd').style.display = 'none';}; document.getElementById('adop_banner_x').addEventListener('click', adop_closebfd);</script>
+</div>
+ 
+<div id='ADOP_V_v90wACpN4d' style='height:0;'></div>
+<script src='https://compasscdn.adop.cc/js/adop_new_collapse_width100_0.0.1.js' data-id='ADOP_V_v90wACpN4d' data-width='300' data-height='250' data-type='re' data-zone='99fde1fa-905c-4449-aee7-bd1e1bbe6eec' data-fl='Y' data-margin='0,120,0,0' data-fl-width='300' data-loc='rb'></script>
+
+`);
+
+// playmall.playpark.com
+$("#playmall_adop").append(` 
+ <div id='adop_bfd' style='display:block;width:100%;position:fixed;bottom:0px;right:0px;z-index:999;text-align:center;background:rgb(255, 255, 255, 0.85);box-shadow:rgba(0, 0, 0, 0.25) 0px 0px 7px;border-top:4px solid #353535;'>
+ <script  src='https://compass.adop.cc/assets/js/adop/adop.js?v=14' ></script><ins class='adsbyadop' _adop_zon = '75eea182-b05a-44f4-bea7-a55f3fd04a27' _adop_type = 'rs' style='display:inline-block;width:320px;height:100px;' _page_url='' _over_size='auto'_over_zone='{"728x90":"682ae9c3-2f83-4f62-a659-1fb8501b41c2"}' ></ins>
+ <span id='adop_banner_x' style='position:absolute;bottom:100%;right:0;padding:2px 8px;background:#060102;font-size:12px;color:#ffffff;border-bottom:4px solid #353535;'>X</span>
+ <script>let adop_closebfd = function () {document.getElementById('adop_bfd').style.display = 'none';}; document.getElementById('adop_banner_x').addEventListener('click', adop_closebfd);</script>
+ </div>
+ `);
+
+// itemshop.playpark.com
+$("#itemshop_adop").append(` 
+<div id='adop_bfd' style='display:block;width:100%;position:fixed;bottom:0px;right:0px;z-index:999;text-align:center;background:rgb(255, 255, 255, 0.85);box-shadow:rgba(0, 0, 0, 0.25) 0px 0px 7px;border-top:4px solid #353535;'>
+<script  src='https://compass.adop.cc/assets/js/adop/adop.js?v=14' ></script><ins class='adsbyadop' _adop_zon = '2244212f-8763-4c04-b244-b50409317ec0' _adop_type = 'rs' style='display:inline-block;width:320px;height:100px;' _page_url='' _over_size='auto'_over_zone='{"728x90":"19141915-0caf-401d-a7db-80e6bffd65b3"}' ></ins>
+<span id='adop_banner_x' style='position:absolute;bottom:100%;right:0;padding:2px 8px;background:#060102;font-size:12px;color:#ffffff;border-bottom:4px solid #353535;'>X</span>
+<script>let adop_closebfd = function () {document.getElementById('adop_bfd').style.display = 'none';}; document.getElementById('adop_banner_x').addEventListener('click', adop_closebfd);</script>
+</div>
+
+
+`);
